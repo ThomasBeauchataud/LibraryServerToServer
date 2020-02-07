3e75b88accaae42eea39b474403e2c824ea3ead1
ffcfalcos <44582903+ffcfalcos@users.noreply.github.com>
2020-02-07 16:49:33 -0500

    Add files via upload

---
+++ Test/ClientTestLibraryServerToServer/Program.cs
@@ -0,0 +1,29 @@
+﻿using LibraryServerToServer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClientTestLibraryServerToServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<object> commandParameters = new List<object>();
+            commandParameters.Add(1);
+            commandParameters.Add(2);
+            Command command = new Command(commandParameters);
+            Flux flux = new Flux();
+            CompressionDecorator compressedFlux = new CompressionDecorator(flux);
+            EncryptionDecorator encryptedCompressedFlux = new EncryptionDecorator(compressedFlux);
+            encryptedCompressedFlux.WriteData(Command.Serialize(command));
+            List<object> parameters = new List<object>();
+            parameters.Add("localhost");
+            parameters.Add(8888);
+            flux = new WebSocketSender().Send(flux, parameters);
+            Console.WriteLine("Command result : " + BitConverter.ToInt32(flux.ReadData(), 0));
+            Console.Write("Type any key to end the program");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Test/ServerTestLibraryServerToServer/Program.cs
@@ -0,0 +1,20 @@
+﻿using LibraryServerToServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerTestLibraryServerToServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IExecutor executor = new CommandExecutor();
+            IAdapter adapter = new CommandAdapter();
+            WebSocketListener webSocketListener = new WebSocketListener(executor, adapter);
+            webSocketListener.StartServer(8888);
+        }
+    }
+}
